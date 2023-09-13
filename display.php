9ef041d4035d3273480e209408bf676a7235653e
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-09-13 23:26:31 +0545

    display code

---
+++ display.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . "<br>";
+        echo "Name: " . $row["name"] . "<br>";
+        echo "Email: " . $row["email"] . "<br>";
+        echo "Created At: " . $row["created_at"] . "<br><br>";
+    }
+} else {
+    echo "No records found.";
+}
+
+$conn->close();
+?>
